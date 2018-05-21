b96ddc1d53b5446e6b5af354533d13167ee80f8e
johneckert <johnteckert@gmail.com>
2018-05-21 17:57:29 -0400

    roll and roll_multiple working

--- lib/nat20.rb
+++ lib/nat20.rb
@@ -1,7 +1,6 @@
 class Nat20
 
-  def self.roll dice_info
-    #eg input = '1d6+4'
+  def self.roll dice_info, return_arr=false #eg input = '1d6+4'
     num_dice = self.get_number_of_dice dice_info
     num_sides = self.get_number_of_sides dice_info
     modifier = self.get_modifier dice_info
@@ -13,20 +12,17 @@ class Nat20
       puts actual_roll
       rolls << actual_roll
     end
-    puts rolls
-    #calculate amount to drop based on lowest dice rolls
-    if self.has_drop? dice_info
-      drop_val = self.num_to_drop dice_info
-      rolls.sort!
-      rolls.slice!(0, drop_val)
+
+    if return_arr
+      return rolls
+    else
+      sum = 0
+      rolls.each {|roll| sum += roll}
+      return sum + modifier
     end
-    sum = 0
-    rolls.each {|roll| sum + roll}
-    sum + modifier
   end
 
-  def self.roll_multiple roll_array
-    #eg input ['1d6+4', 5]
+  def self.roll_multiple roll_array #eg input ['1d6+4', 5]
     output = []
     roll_array[1].times do
       roll = self.roll roll_array[0]
@@ -54,14 +50,6 @@ class Nat20
       modifier = 0
     end
     modifier
-  end
-
-  def self.has_drop? dice_info
-    dice_info.split('d').length == 3
-  end
-
-  def self.num_to_drop dice_info
-    dice_info.split('d')[2][0].to_i
   end
 
 end
