37392301494c9cdb052d40eefd5b03239c1173b5
johneckert <johnteckert@gmail.com>
2018-05-21 13:05:07 -0400

    building methods

--- lib/nat20.rb
+++ lib/nat20.rb
@@ -2,21 +2,27 @@ class Nat20
 
   def self.roll dice_info
     #eg input = '1d6+4'
-      num_dice = self.get_number_of_dice dice_info
-      num_sides = self.get_number_of_sides dice_info
-    if dice_info.include? '+'
-      modifier = dice_info.split('d')[1].split('+')[1].to_i
-    elsif dice_info.include? '-'
-      modifier = 0 - dice_info.split('d')[1].split('-')[1].to_i
-    else
-      modifier = 0
-    end
+    num_dice = self.get_number_of_dice dice_info
+    num_sides = self.get_number_of_sides dice_info
+    modifier = self.get_modifier dice_info
 
-    total = 0
+    rolls = []
+
     num_dice.times do
-      total += 1 + rand(num_sides)
+      actual_roll = 1 + rand(num_sides)
+      puts actual_roll
+      rolls << actual_roll
     end
-    total + modifier
+    puts rolls
+    #calculate amount to drop based on lowest dice rolls
+    if self.has_drop? dice_info
+      drop_val = self.num_to_drop dice_info
+      rolls.sort!
+      rolls.slice!(0, drop_val)
+    end
+    sum = 0
+    rolls.each {|roll| sum + roll}
+    sum + modifier
   end
 
   def self.roll_multiple roll_array
@@ -40,6 +46,22 @@ class Nat20
   end
 
   def self.get_modifier dice_info
-    
+    if dice_info.include? '+'
+      modifier = dice_info.split('d')[1].split('+')[1].to_i
+    elsif dice_info.include? '-'
+      modifier = 0 - dice_info.split('d')[1].split('-')[1].to_i
+    else
+      modifier = 0
+    end
+    modifier
+  end
+
+  def self.has_drop? dice_info
+    dice_info.split('d').length == 3
+  end
+
+  def self.num_to_drop dice_info
+    dice_info.split('d')[2][0].to_i
+  end
 
 end
